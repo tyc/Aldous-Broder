5b300fb102d9464ccc83827b8bf8b46c74b04af2
tehn.chin <tehn.chin@yazaki-europe.com>
2016-03-14 14:29:41 +0100

    got the array of struct initialise, sort of

--- Aldous-Broder/src/main.rs
+++ Aldous-Broder/src/main.rs
@@ -10,7 +10,7 @@ use rand::Rng;
 //
 
 struct Cell {
-	visted: bool,
+	visited: u8,
 	carve: u8, 
 }
 
@@ -18,17 +18,31 @@ struct Cell {
 
 fn main() {
 	
-	let mut grid:[i8; 5] : Cell;
+	let mut grid : [Cell; 2] = 
+        [
+            Cell{visited:0x01, carve:0x01},
+            Cell{visited:0x01, carve:0x01},
+         ];
 	
+    let mut grid1 :Cell;
+
+    // The following line complains that a copy implementation is
+    // available for Cell.
+    let mut grid2 : [Cell; 100] = [ Cell{visited:0x01, carve:0x01}; 100 ];
+
+
 //	let secret_number = rand::thread_rng().gen_range(1,101);
 //		
 //	println!("The secret number is : {}", secret_number);
 
     let width = 10;
     let height = 10;
-    
-    grid[0].visted = true;
+   
+    grid[0].visited = 0x01;
     grid[0].carve = 0x01;
+
+    grid1.visited = 0x01;
+    grid1.carve = 0x01;
     
     let x = rand::thread_rng().gen_range(1,width);
     let y = rand::thread_rng().gen_range(1,height);
